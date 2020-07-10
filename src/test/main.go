b19c400d150892230dddadeab3e8f79b864c61ec
jqktiankong <jqk_tiankong@126.com>
2020-07-10 22:06:09 +0800

    文件存储

--- src/test/main.go
+++ src/test/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/gob"
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"html/template"
@@ -339,42 +341,155 @@ func process10(w http.ResponseWriter, r *http.Request) {
 //	server.ListenAndServe()
 //}
 
+//type Post struct {
+//	Id      int
+//	Content string
+//	Author  string
+//}
+//
+//var PostById map[int]*Post
+//var PostsByAuthor map[string][]*Post
+//
+//func store(post Post) {
+//	PostById[post.Id] = &post
+//	PostsByAuthor[post.Author] = append(PostsByAuthor[post.Author], &post)
+//}
+//
+//func main() {
+//	PostById = make(map[int]*Post)
+//	PostsByAuthor = make(map[string][]*Post)
+//
+//	post1 := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
+//	post2 := Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"}
+//	post3 := Post{Id: 3, Content: "Hola Mundo!", Author: "Pedro"}
+//	post4 := Post{Id: 4, Content: "Greetings Earthlings", Author: "Sau Sheong"}
+//
+//	store(post1)
+//	store(post2)
+//	store(post3)
+//	store(post4)
+//
+//	fmt.Println(PostById[1])
+//	fmt.Println(PostById[2])
+//
+//	for _, post := range PostsByAuthor["Sau Sheong"] {
+//		fmt.Println(post)
+//	}
+//
+//	for _, post := range PostsByAuthor["Pedro"] {
+//		fmt.Println(post)
+//	}
+//}
+
+//func main()  {
+//	data := []byte("Hello World!\n")
+//	err := ioutil.WriteFile("data1", data, 0644)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	read1, _ := ioutil.ReadFile("data1")
+//	fmt.Print(string(read1))
+//
+//	file1, _ := os.Create("data2")
+//	defer file1.Close()
+//
+//	bytes, _ := file1.Write(data)
+//	fmt.Printf("Wrote %d bytes to file\n", bytes)
+//
+//	file2, _ := os.Open("data2")
+//	defer file2.Close()
+//
+//	read2 := make([]byte, len(data))
+//	bytes, _ = file2.Read(read2)
+//	fmt.Printf("Read %d bytes from file\n", bytes)
+//	fmt.Println(string(read2))
+//}
+
 type Post struct {
 	Id      int
 	Content string
 	Author  string
 }
 
-var PostById map[int]*Post
-var PostsByAuthor map[string][]*Post
+func store(data interface{}, filename string) {
+	buffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(buffer)
+	err := encoder.Encode(data)
+	if err != nil {
+		panic(err)
+	}
+	err = ioutil.WriteFile(filename, buffer.Bytes(), 0600)
+	if err != nil {
+		panic(err)
+	}
+}
 
-func store(post Post) {
-	PostById[post.Id] = &post
-	PostsByAuthor[post.Author] = append(PostsByAuthor[post.Author], &post)
+func load(data interface{}, filename string) {
+	raw, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	buffer := bytes.NewBuffer(raw)
+	dec := gob.NewDecoder(buffer)
+	err = dec.Decode(data)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func main() {
-	PostById = make(map[int]*Post)
-	PostsByAuthor = make(map[string][]*Post)
+	//csvFile, err := os.Create("posts.csv")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//defer csvFile.Close()
+	//
+	//allPosts := []Post{
+	//	Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"},
+	//	Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"},
+	//	Post{Id: 3, Content: "Hola Mundo!", Author: "pedro"},
+	//	Post{Id: 4, Content: "Greetings Earthings!", Author: "Sau Sheong"},
+	//}
+	//
+	//writer := csv.NewWriter(csvFile)
+	//for _, post := range allPosts {
+	//	line := []string{strconv.Itoa(post.Id), post.Content, post.Author}
+	//	err := writer.Write(line)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+	//}
+	//writer.Flush()
+	//
+	//file, err := os.Open("posts.csv")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//defer file.Close()
+	//
+	//reader := csv.NewReader(file)
+	//reader.FieldsPerRecord = -1
+	//record, err := reader.ReadAll()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//var posts []Post
+	//for _, item := range record {
+	//	id, _ := strconv.ParseInt(item[0], 0,0)
+	//	post := Post{Id: int(id), Content: item[1], Author: item[2]}
+	//	posts = append(posts, post)
+	//}
+	//fmt.Println(posts[0].Id)
+	//fmt.Println(posts[0].Content)
+	//fmt.Println(posts[0].Author)
 
-	post1 := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
-	post2 := Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"}
-	post3 := Post{Id: 3, Content: "Hola Mundo!", Author: "Pedro"}
-	post4 := Post{Id: 4, Content: "Greetings Earthlings", Author: "Sau Sheong"}
-
-	store(post1)
-	store(post2)
-	store(post3)
-	store(post4)
-
-	fmt.Println(PostById[1])
-	fmt.Println(PostById[2])
-
-	for _, post := range PostsByAuthor["Sau Sheong"] {
-		fmt.Println(post)
-	}
-
-	for _, post := range PostsByAuthor["Pedro"] {
-		fmt.Println(post)
-	}
+	post := Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"}
+	store(post, "post1")
+	var postRead Post
+	load(&postRead, "post1")
+	fmt.Println(postRead)
 }
